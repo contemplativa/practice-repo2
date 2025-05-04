da45baa2878bcbb3bbb30b6ba8daf2721f6b2fe0
contemplativa <thewantandthedo@gmail.com>
2025-05-04 14:12:51 +0800

    isDigit improved

--- sort.cpp
+++ sort.cpp
@@ -51,20 +51,36 @@ bool isDigit(std::string string){
     char nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     bool digit = false;
     int arraySize = sizeof(nums)/sizeof(nums[0]);
+    //test
+    std::cout << "size: " << arraySize << "\n";
     for(int i = 0; i < string.length(); i++){
         for(int j = 0; j < arraySize; j++){
+            // test2
+            std::cout << "string.at(i): " << string.at(i) << " | nums[j]: " << nums[j] << "\n"; 
             if(string.at(i) == nums[j]){
+                //message
+                std::cout << "matched\n";
                 digit = true;
+                std::cout << digit << "\n";
                 break;
             }
             else{
                 if(j == arraySize-1){
+                    //message
+                    std::cout << "returned immediately.";
+                    std::cout << digit << "\n";
                     return false;
                     break;
                 }
+                //message
+                std::cout << "did not match.\n";
+                digit = false;
+                std::cout << digit << "\n";
                 continue;
             }
         }
     }
+    //message
+    std::cout << "digit has been returned: " << digit << "\n";
     return digit;
 }
