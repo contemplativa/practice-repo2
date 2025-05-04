9294962e9764ac6fd980ac6a344da1625d93bf45
contemplativa <thewantandthedo@gmail.com>
2025-05-04 12:22:59 +0800

    Added asking user input for sort type
    sort.cpp file

--- sort.cpp
+++ sort.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 int main(int argc, char* argv[]){
     int size = 0;
+    char type;
     do{
     std::cout << "Number Sorting Algorithms";
     std::cout << "Size of array: ";
     std::cin >> size;
     }while(size < 0);
     int* pN = new int[size];
-    
+
+    do{
+        std::cout << "Sorting Algorithm to use\n";
+        std::cout << "'b' for bubble sort\n";
+        std::cout << "'s' for selection sort\n";
+        std::cin >> type;
+        if(type != 'b' || type != 's'){
+            std::cout << "Type must be 'b' or 's'. Please try again.\n";
+        }
+    }while(type != 'b' || type != 's');
 }
