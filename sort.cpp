189a29493a6a06b41fd678611da0415e64fa0ef4
contemplativa <thewantandthedo@gmail.com>
2025-05-04 12:16:23 +0800

    Added basic user input to sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
 int main(int argc, char* argv[]){
+    int size = 0;
+    do{
+    std::cout << "Number Sorting Algorithms";
+    std::cout << "Size of array: ";
+    std::cin >> size;
+    }while(size < 0);
+    int* pN = new int[size];
     
 }
