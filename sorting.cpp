0ae5a0b87ae21770e7c880633307107305db3024
contemplativa <thewantandthedo@gmail.com>
2025-05-04 12:35:39 +0800

    Created sorting.h and sorting.cpp
    Structure has been created. Bubble and selection sort is to be implemented in the future.

---
+++ sorting.cpp
@@ -0,0 +1,9 @@
+#include "sorting.h"
+
+// sorting class
+void Sorting::bubble(){
+
+}
+void Sorting::selection(){
+    
+}
---
+++ sorting.h
@@ -0,0 +1,10 @@
+#ifndef SORTING_H
+#define SORTING_H
+class Sorting{
+public:
+    void bubble();
+    
+    void selection();
+};
+
+#endif
